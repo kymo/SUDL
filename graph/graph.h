4a6174fc89c80237f68098063136403048673178
aron <412062385@qq.com>
2017-09-29 16:43:13 +0800

    fix graph bugs

--- graph/graph.h
+++ graph/graph.h
@@ -19,6 +19,23 @@
 #include "util.h"
 #include "matrix.h"
 #include "layer.h"
+#include "loss_layer.h"
+#include "seq_full_conn_layer.h"
+#include "seq_full_conn_softmax_layer.h"
+#include "seq_loss_layer.h"
+#include "rnn_cell.h"
+#include "lstm_cell.h"
+#include "gru_cell.h"
+#include "bi_cell_wrapper.h"
+#include "matrix.h"
+#include "embedding_layer.h"
+#include "conv_layer.h"
+#include "pooling_layer.h"
+#include "full_conn_layer.h"
+#include "full_conn_softmax_layer.h"
+#include "active_layer.h"
+#include "loss_layer.h"
+#include "util.h"
 #include <queue>
 
 namespace sub_dl {
@@ -42,11 +59,17 @@ typedef struct node {
     }
 
     void _forward() {
-        _layer->_forward(_pre_layers[0]);
+        if (_pre_layers.size() > 0) {
+            _layer->_forward(_pre_layers[0]);
+        }
     }
 
     void _backward() {
-        _layer->_backward(_nxt_layers[0]);
+        if (_nxt_layers.size() > 0) {
+            _layer->_backward(_nxt_layers[0]);
+        } else {
+            _layer->_backward(NULL);
+        }
     }
 
     void _update_gradient() {
@@ -62,34 +85,155 @@ typedef struct node {
 class Graph {
 
 private:
-    std::map<int, Layer*> _layer_map;
+
     std::map<int, std::vector<int> > _edges;
     std::map<int, std::vector<int> > _reverse_edges;
+    
+    std::vector<int> _input_nodes;
     std::map<int, Operator*> _op_map;
+    
     std::map<int, int> _node_ins;
     std::map<int, int> _reverse_node_ins;
 
     std::queue<int> _queue;
     bool _visited;
     int _node_cnt;
+
 public:    
     Graph() {
         _node_cnt = 0;
     }
+    
+    double _gradient_check(const std::vector<std::vector<matrix_double> >& batch_x,
+        const std::vector<matrix_double>& batch_y) {
+
+        std::vector<Layer*> _layers;
+        for (auto it = _op_map.begin(); it != _op_map.end(); it++) {
+            _layers.push_back(it->second->_layer);
+        }
+
+        for (int l = 0; l < _layers.size(); l++) {
+            if (_layers[l]->_type == SEQ_FULL) {
+                std::cout << "------------Gradient Check for seq full layer -------------" << std::endl;
+                SeqFullConnLayer* seq_full = (SeqFullConnLayer*) _layers[l];
+                _gradient_weights_check(batch_x, batch_y, 
+                    seq_full->_seq_full_weights, seq_full->_delta_seq_full_weights);
+                _gradient_weights_check(batch_x, batch_y, 
+                    seq_full->_seq_full_bias, seq_full->_delta_seq_full_bias);
+            } else if (_layers[l]->_type == RNN_CELL) {
+                std::cout << "------------Gradient Check for rnn cell layer -------------" << std::endl;
+                RnnCell* rnn_cell = (RnnCell*) _layers[l];
+                _gradient_weights_check(batch_x, batch_y, 
+                    rnn_cell->_input_hidden_weights, rnn_cell->_delta_input_hidden_weights); 
+            } else if (_layers[l]->_type == LSTM_CELL) {
+                std::cout << "------------Gradient Check for lstm cell layer -------------" << std::endl;
+                LstmCell* lstm_cell = (LstmCell*) _layers[l];
+                _gradient_weights_check(batch_x, batch_y, 
+                    lstm_cell->_ig_input_weights, lstm_cell->_ig_delta_input_weights);
+            } else if (_layers[l]->_type == BI_LSTM_CELL) {
+                std::cout << "------------Gradient Check for bi lstm cell layer -------------" << std::endl;
+                LstmCell* lstm_cell = ((BiCellWrapper<LstmCell>*) _layers[l])->_pos_seq_cell;
+                _gradient_weights_check(batch_x, batch_y, 
+                    lstm_cell->_ig_input_weights, lstm_cell->_ig_delta_input_weights);
+                _gradient_weights_check(batch_x, batch_y, lstm_cell->_og_input_weights, lstm_cell->_og_delta_input_weights);
+                _gradient_weights_check(batch_x, batch_y, lstm_cell->_fg_input_weights, lstm_cell->_fg_delta_input_weights);
+                _gradient_weights_check(batch_x, batch_y, lstm_cell->_cell_input_weights, lstm_cell->_cell_delta_input_weights);
+            } else if (_layers[l]->_type == BI_RNN_CELL) {
+                std::cout << "------------Gradient Check for bi rnn cell layer -------------" << std::endl;
+                RnnCell* rnn_cell = ((BiCellWrapper<RnnCell>*) _layers[l])->_pos_seq_cell;
+                _gradient_weights_check(batch_x, batch_y, rnn_cell->_input_hidden_weights, rnn_cell->_delta_input_hidden_weights);
+                _gradient_weights_check(batch_x, batch_y, rnn_cell->_hidden_weights, rnn_cell->_delta_hidden_weights);
+            } else if (_layers[l]->_type == BI_GRU_CELL) {
+                std::cout << "------------Gradient Check for bi gru cell layer -------------" << std::endl;
+                GruCell* gru_cell = ((BiCellWrapper<GruCell>*) _layers[l])->_pos_seq_cell;
+                _gradient_weights_check(batch_x, batch_y, gru_cell->_ug_hidden_weights, gru_cell->_delta_ug_hidden_weights); 
+                _gradient_weights_check(batch_x, batch_y, gru_cell->_rg_hidden_weights, gru_cell->_delta_rg_hidden_weights); 
+                _gradient_weights_check(batch_x, batch_y, gru_cell->_newh_hidden_weights, gru_cell->_delta_newh_hidden_weights); 
+                _gradient_weights_check(batch_x, batch_y, gru_cell->_newh_input_weights, gru_cell->_delta_newh_input_weights); 
+
+            } else if (_layers[l]->_type == GRU_CELL) {
+                std::cout << "------------Gradient Check for gru cell layer -------------" << std::endl;
+                GruCell* gru_cell = (GruCell*) _layers[l];
+                _gradient_weights_check(batch_x, batch_y, gru_cell->_ug_hidden_weights, gru_cell->_delta_ug_hidden_weights); 
+                _gradient_weights_check(batch_x, batch_y, gru_cell->_rg_hidden_weights, gru_cell->_delta_rg_hidden_weights); 
+                _gradient_weights_check(batch_x, batch_y, gru_cell->_newh_hidden_weights, gru_cell->_delta_newh_hidden_weights); 
+                _gradient_weights_check(batch_x, batch_y, gru_cell->_newh_input_weights, gru_cell->_delta_newh_input_weights); 
+            } else if (_layers[l]->_type == CONV) {
+                ConvLayer* layer = (ConvLayer*) _layers[l];
+                std::cout << "-------Conv Layer Gradient Check Result ---------" << std::endl;
+                for (int i = 0; i < layer->_input_dim;i ++) {
+                    for (int j = 0; j < layer->_output_dim; j++) {
+                        if (layer->_conn_map[i][j]) {
+                            _gradient_weights_check(batch_x, batch_y, 
+                                layer->_conv_kernels[i][j], layer->_delta_conv_kernels[i][j]);
+                         }
+                    }
+                }
+            } else if (_layers[l]->_type == FULL_CONN) {
+                BaseFullConnLayer* layer = (BaseFullConnLayer*) _layers[l];
+                std::cout << "-------Full Layer Gradient Check Result ---------" << std::endl;
+                _gradient_weights_check(batch_x, batch_y, layer->_full_conn_weights, layer->_delta_full_conn_weights);
+
+            }
+        }
+    }
+    
+    void _gradient_weights_check(const std::vector<std::vector<matrix_double> >& batch_x,
+        const std::vector<matrix_double>& batch_y,
+        matrix_double& weights,
+        const matrix_double& delta_weights) {
+        for (int i = 0; i < weights._x_dim; i++) {
+            for (int j = 0; j < weights._y_dim; j++) {
+                
+                double v = weights[i][j];
+                weights[i][j] = v + 1.0e-4;
+                double f1 = 0.0;
+                double f2 = 0.0;
+                for (int k = 0; k < batch_x.size(); k++) {    
+                    matrix_double label;
+                    label_encode(batch_y[k], label, 4);
+                    _set_input(batch_x[k]);
+                    _set_label(label);
+                    f1 += _forward_compute();
+                }
+                weights[i][j] = v - 1.0e-4;
+                for (int k = 0; k < batch_x.size(); k++) {
+                    matrix_double label;
+                    label_encode(batch_y[k], label, 4);
+                    _set_input(batch_x[k]);
+                    _set_label(label);
+                    f2 += _forward_compute(); 
+                }
+                std::cout << "[ " << delta_weights[i][j] << "," << (f1 - f2) / (2.0e-4) << "]";
+                weights[i][j] = v;
+            }
+        }
+    }
+
 
     // add a node into graph, and build the edges between the node and
     // the other nodes in id_vec
     int _add_node(Layer* layer, std::vector<int> id_vec) {
+
+        // what add is the input  layer
+        if (id_vec.size() == 0) {
+            ++_node_cnt;
+            _input_nodes.push_back(_node_cnt);
+            _op_map[_node_cnt] = new Operator(layer, _node_cnt);
+            _node_ins[_node_cnt] = 0;
+            return _node_cnt;
+        }
+
         // _operators.push_back(new Operator(layer, ++_node_cnt));
         ++_node_cnt;
-        
+      
         _op_map[_node_cnt] = new Operator(layer, _node_cnt);
         
         if (_reverse_edges.find(_node_cnt) == _reverse_edges.end()) {
             _reverse_edges[_node_cnt] = std::vector<int>();
         }
         _node_ins[_node_cnt] = id_vec.size();
-
+        _reverse_node_ins[_node_cnt] = 0;
         // build the connection from the forward direction 
         for (auto id : id_vec) {
             if (_edges.find(id) == _edges.end()) {
@@ -106,11 +250,48 @@ public:
             _op_map[_node_cnt]->_add_pre_layers(_op_map[id]->_layer);
             _op_map[id]->_add_nxt_layer(layer);
         }
-
         return _node_cnt;
     }
 
-    void _forward_compute() {
+    void _set_input(const std::vector<matrix_double>& feature) {
+        DataFeedLayer* input_layer = (DataFeedLayer*)_op_map[_input_nodes[0]]->_layer;
+        input_layer->_set_data(feature);
+    }
+
+    void _set_label(const matrix_double& label) {
+        LossLayer* layer = (LossLayer*) _op_map[_node_cnt]->_layer;
+        layer->_set_label(label);
+    }
+
+    void _run(std::vector<std::vector<matrix_double> >& batch_x,
+        std::vector<matrix_double> batch_y, int _output_dim) {
+
+        _clear_gradient();
+        for (int i = 0; i < batch_x.size(); i++) {
+            matrix_double label;
+            _set_input(batch_x[i]);
+            label_encode(batch_y[i], label, _output_dim);
+            _set_label(label);
+            _forward_compute();
+            _backward_compute();
+        }
+        _gradient_check(batch_x, batch_y);
+        _update_gradient();
+    }
+
+    void _clear_gradient() {
+        for (auto it = _op_map.begin(); it != _op_map.end(); it++) {
+            it->second->_clear_gradient();
+        }
+    }
+
+    void _update_gradient() {
+        for (auto it = _op_map.begin(); it != _op_map.end(); it++) {
+            it->second->_update_gradient();
+        }
+    }
+
+    double _forward_compute() {
         // id=0 is the input layer
         std::map<int, int> node_ins(_node_ins.begin(), _node_ins.end());
         for (auto it = node_ins.begin(); it != node_ins.end(); it++) {
@@ -118,11 +299,11 @@ public:
                 _queue.push(it->first);
             }
         }
+
         while (! _queue.empty()) {
             int fid = _queue.front();
             _op_map[fid]->_forward();
             _queue.pop();
-
             if (_edges.find(fid) == _edges.end()) {
                 continue;
             }
@@ -133,11 +314,22 @@ public:
                 }
             }
         }
+        
+        double cost = 0.0;
+        LossLayer* layer = (LossLayer*) _op_map[_node_cnt]->_layer;
+        for (auto vec : layer->_data) {
+            cost += vec.sum();
+        }
+        return cost;
     }
+
+
 
     void _backward_compute() {
         // id ==
-        std::map<int, int> node_ins(_reverse_node_ins.begin(), _node_ins.end());
+        
+        std::map<int, int> node_ins(_reverse_node_ins.begin(), _reverse_node_ins.end());
+        
         for (auto it = node_ins.begin(); it != node_ins.end(); it++) {
             if (it->second == 0) {
                 _queue.push(it->first);
@@ -150,7 +342,7 @@ public:
             if (_reverse_edges.find(fid) == _reverse_edges.end()) {
                 continue;
             }
-            for (auto id : _edges[fid]) {
+            for (auto id : _reverse_edges[fid]) {
                 node_ins[id] --;
                 if (0 == node_ins[id]) {
                     _queue.push(id);
@@ -158,6 +350,8 @@ public:
             }
         }
     }
+
+
 
 };
 }
--- graph/test_graph.cpp
+++ graph/test_graph.cpp
@@ -1,35 +1,102 @@
 
 #include "graph.h"
-#include "embedding_layer.h"
-#include "seq_full_conn_softmax_layer.h"
-
 #include <stdarg.h> 
+#include <string>
+#include <fstream>
+#include <vector>
+#include <map>
 
 using namespace sub_dl;
 
 std::vector<int> REFER(int n, ...) {
+    std::vector<int> ret;
+	if (n == 0) {
+		return ret;
+	}
+
     va_list args;
     va_start(args, n);
-    std::vector<int> ret;
     while (n > 0) {
         ret.push_back(va_arg(args, int));
         n --;
     }
     return ret;
 }
+#define SAMPLE_SEP ";"
+#define FEATURE_SEP " "
+#define LABEL_SEP " "
+#define SAMPLE_SEP_SIZE 2
+
+void load_data(const char*file_name,
+    std::vector<std::vector<matrix_double> >& _train_x_features,
+    std::vector<matrix_double>& _train_y_labels) {
+
+    std::ifstream fis(file_name);
+    std::string line;
+
+    while (getline(fis, line)) {
+        std::vector<std::string> split_strs;
+        split(line, SAMPLE_SEP, split_strs);
+        if (split_strs.size() != SAMPLE_SEP_SIZE) {
+            std::cerr << "Error when load feature!" << std::endl;
+            exit(1);
+        }
+        std::vector<std::string> features;
+        std::vector<std::string> labels;
+        split(split_strs[0], FEATURE_SEP, features);
+        split(split_strs[1], LABEL_SEP, labels);
+        if (features.size() != labels.size()) {
+            std::cerr << "Error when load feature: size of features are \
+                not match with size of labels!" << std::endl;
+            exit(1);
+        }
+        std::vector<matrix_double> feature(features.size(), matrix_double(1, 1));
+        matrix_double label(features.size(), 1);
+        for (int i = 0; i < features.size(); i++) {
+            feature[i][0][0] = atoi(features[i].c_str());
+            // feature[0][i] = atoi(features[i].c_str());
+            label[i][0] = atoi(labels[i].c_str());
+        }
+        _train_x_features.push_back(feature);
+        _train_y_labels.push_back(label);
+    }
+    std::cout << "Load data done!" << std::endl;
+}
 
 int main() {
     
     Graph *graph = new Graph();
-
-    int emb_id = graph->_add_node(new WordEmbeddingLayer(14), REFER(1, 0));
-    
+	int input_1 = graph->_add_node(new DataFeedLayer(), REFER(0));
+    int emb_id = graph->_add_node(new WordEmbeddingLayer(14), 
+		REFER(1, input_1)); // input_2));
     int lstm_id = graph->_add_node(new LstmCell(14, 16, true), REFER(1, emb_id)); 
     int seq_full_id = graph->_add_node(new SeqFullConnSoftmaxLayer(16, 4), REFER(1, lstm_id));
+	int loss_layer = graph->_add_node(new SeqCrossEntropyLossLayer(), REFER(1, seq_full_id));
     
-    int seq_full_id1 = graph->_add_node(new SeqFullConnSoftmaxLayer(16, 4), REFER(2, emb_id, seq_full_id));
+	std::vector<std::vector<matrix_double> > train_x_features;
+    std::vector<matrix_double> train_y_labels;
+    load_data("train_text.seg.10w", train_x_features, train_y_labels);
+    int _max_epoch_cnt = 1000;
+    int batch_size = 50;
+    int tot = 500;
+	clock_t startTime,endTime;
+    for (int epoch = 0; epoch < _max_epoch_cnt; epoch++) {
+		startTime = clock();
+        for (int i = 0; i < tot / batch_size; i++) {
+            double cost = 0.0;
+            std::string val1, val2;
+            std::vector<std::vector<matrix_double> > batch_x_features;
+            std::vector<matrix_double> batch_y_labels;
+            for (int j = i * batch_size; j < (i + 1) * batch_size 
+                && j < train_y_labels.size(); j++) {
+                batch_x_features.push_back(train_x_features[j]);
+                batch_y_labels.push_back(train_y_labels[j]);
+            }
+        	graph->_run(batch_x_features, batch_y_labels, 4);
+		}
+		endTime = clock();
+		std::cout << "Totle Time : " <<(double)(endTime - startTime) * 1000 / CLOCKS_PER_SEC << "ms" << std::endl;
+    }
 
-    graph->_forward_compute();
-
-    return 0;
+	return 0;
 }
